0d0a443ba9fac440feca86198c858c7f9745b4b8
김다영 <2dayomme@gmail.com>
2023-08-01 17:02:42 +0900

    BoardActivity

--- frontend/app/src/main/java/com/hjy/hackathon/mainfragment/MainFragment.kt
+++ frontend/app/src/main/java/com/hjy/hackathon/mainfragment/MainFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hjy.hackathon.R
-import com.hjy.hackathon.VO.MainVO
-import com.hjy.hackathon.databinding.ActivityMainBinding
+import com.hjy.hackathon.vo.MainVO
 import com.hjy.hackathon.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
