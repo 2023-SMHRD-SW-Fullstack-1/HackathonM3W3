a66a2a4dbf99c770853f607ce5ef34dab275ccbc
HwangJeongyong <jeongyong62@gmail.com>
2023-08-04 06:03:06 +0900

    node

--- backend/routes/calendar.js
+++ backend/routes/calendar.js
@@ -9,38 +9,15 @@ db.connect(conn);
 
 
 router.post("/",(req, res)=>{
-    
-    // console.log(JSON.parse(req.body.Andmember));
 
-    var result = JSON.parse(req.body.Andmember)
     let {date, id} = JSON.parse(req.body.Andmember)
 
-    // var year = date.split("-")[0]
-    // var month = date.split("-")[1]
-    // month = parseInt(month)
-    //console.log(year);
-    //console.log(month);
-
-    // var date2 = year + "-" + month
-    // console.log("123" + date2);
-    
-    //let sql = "select board_cost, board_content, board_at from tb_board where board_at LIKE ? "
-    //let sql = "select * from tb_board"
     let sql = "SELECT board_at, SUM(board_cost) AS total_cost FROM tb_board WHERE board_at LIKE ? and mb_id = ? GROUP BY board_at order by board_at"
 
-    conn.query(sql,[date.substr(0,7)+'%', id], (err,rows)=>{
+    conn.query(sql,[date.substr(0,4)+'%', id], (err,rows)=>{
         
         console.log(rows);
 
-        // var sum = 0;
-
-        // for (const row of rows) {
-        //     //console.log(row.board_cost);
-        //     sum += row.board_cost
-        // }
-        
-        //console.log(sum);
-    
         if (err) {
             console.log(err);
             res.send("Fail");
@@ -77,6 +54,24 @@ router.post('/dayMoney', (req, res)=>{
 // })
 
 })
+
+router.post("/day", (req, res)=>{
+    let {date, id} = JSON.parse(req.body.calendar);
+
+    let sql = "SELECT * FROM tb_board WHERE board_at = ? and mb_id = ?"
+
+    conn.query(sql,[date, id], (err,rows)=>{
+        
+        console.log(rows);
+
+        if (err) {
+            console.log(err);
+            res.send("Fail");
+        } else {
+            res.send(rows);
+        }
+    })
+});
 
 
 
