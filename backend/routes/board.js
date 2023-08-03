3bc0aa9ba7dd4e90eebc97977c09d37c1b179350
jdw2026 <ekekek2026@gmail.com>
2023-08-03 20:46:58 +0900

    lskdfgjsdlfkjsdlg

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -2,7 +2,6 @@ const express = require("express");
 const {v4:uuidv4} =require('uuid')
 const fs = require('fs')
 const db = require("../config/database");
-const fs = require("fs");
 const router = express.Router();
 
 const conn = db.init();
